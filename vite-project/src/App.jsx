8721cf085e9b13c67f0a84ccac514f9ddf332750
vidhyasagar135 <2203A51698@sru.edu.in>
2025-09-07 13:08:57 +0530

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Dashboard from './Components/Dashboard'
+import LandingPage1 from './Components/LandingPage1'
 function App() {
   
 
   return (
     <>
       <Dashboard />
+      <LandingPage1/>
     </>
   )
 }
